dd71b592f23ad73257397eac79e976fe349690b7
Vidi Ramadhandy <dhandy301102@gmail.com>
2025-04-27 23:32:56 +0700

    Update 27 April 2025 (Error 419 on web GET 200 on Postman)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+// Route untuk registrasi
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+// Route untuk login
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
 // Route yang dilindungi oleh middleware auth:sanctum
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
