1113343436f585eaac6ad347a97c22a1a59142e8
Vidi Ramadhandy <dhandy301102@gmail.com>
2025-05-05 01:46:32 +0700

    Updated Homepage and navbar

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [RegisteredUserController::class, 'store']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -1,8 +1,7 @@
 'use client';
 
-import ReactLoremIpsum from 'react-lorem-ipsum';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/navbar';
-import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 export default function Home() {
@@ -22,26 +21,28 @@ export default function Home() {
     };
   }, []);
 
-  const blurAmount = Math.min(10, scrollY / 100); // Mengatur tingkat blur berdasarkan scroll
+  // Menetapkan brightness ke 1 hanya di posisi paling atas, dan 0 (gelap) saat scroll dimulai
+  const brightnessAmount = scrollY > 0.175 ? 0.175 : 0.55;
 
   return (
     <div className="relative min-h-screen bg-black text-white">
       {/* Navbar */}
       <Navbar />
 
-      {/* Background Image dengan efek blur yang tergantung scroll */}
+      {/* Background Image dengan efek brightness yang tergantung scroll */}
       <div
         className="absolute inset-0 z-0"
         style={{
           backgroundImage: "url('/background.jpg')", // Ganti dengan gambar yang sesuai
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-          filter: `brightness(0.375) saturate(0.75) sepia(0.25) contrast(1.25) blur(${blurAmount}px)`,
+          filter: `brightness(${brightnessAmount}) saturate(0.75) sepia(0.25) contrast(1.25)`, // Mengubah brightness
           position: 'fixed',
           top: 0,
           left: 0,
           right: 0,
           bottom: 0,
+          transition: 'filter 500ms ease-in-out', // Menambahkan transisi 500ms
         }}
       ></div>
 
@@ -51,32 +52,27 @@ export default function Home() {
         <main className="text-left px-6 md:px-12 lg:px-16 p-4">
           <div className="max-w-6xl mx-auto">
             {/* Judul */}
-            <h1 className="text-9xl font-extrabold mb-8 text-white font-title ml-0">
+            <h1 className="text-9xl font-extrabold mb-8 text-white font-title ml-0 animate__animated animate__fadeInUp transition-all duration-300">
               About OptiPredict
             </h1>
 
             {/* Teks Penjelasan */}
-            <div className="text-lg text-white leading-relaxed max-w-5xl mx-auto ml-0">
-              OptiPredict hadir sebagai solusi inovatif berbasis machine learning untuk pemeliharaan dan manajemen gangguan pada jaringan optik. Dengan memanfaatkan teknologi machine learning yang terintregrasi, OptiPredict memungkinkan pengguna menganalisis data otomatis serta memprediksi berbagai gangguan dengan tingkat akurasi yang tinggi. 
-              <br/>
-              <br/>
+            <div className="text-lg text-white leading-relaxed max-w-5xl mx-auto ml-0 animate__animated animate__fadeInUp">
+              OptiPredict hadir sebagai solusi inovatif berbasis machine learning untuk pemeliharaan dan manajemen gangguan pada jaringan optik. Dengan memanfaatkan teknologi machine learning yang terintegrasi, OptiPredict memungkinkan pengguna menganalisis data otomatis serta memprediksi berbagai gangguan dengan tingkat akurasi yang tinggi.
+              <br />
+              <br />
               Topologi Dasar OptiPredict:
-              <br/>
-              <Image 
-                src="/topology.jpg" 
-                alt="Image1" 
-                width={1000} 
-                height={600} 
+              <br />
+              <Image
+                src="/topology.jpg"
+                alt="Image1"
+                width={1000}
+                height={600}
               />
-              <br />
-              <br />
+
               Penjelasan Topologi
               <br />
-              <br />
-              With OptiPredict, you get the insights you need to stay ahead of the competition and make smarter, data-driven choices that can propel your business to the next level.
-              <br />
-              <br />
-              Platform ini didukung dengan antarmuka pengguna yang responsif dan memungkinkan akses cepat pemantauan kondisi jaringan optik dengan laporan yang mudah dipahami. Fitur manajemen data dan pelaporan memungkinkan pengguna untuk menyimpan riwayat prediksi dan mengoptimalkan proses pengambilan keputusan terkait pemeliharaan jaringan, sehingga mencegah kerusakan sebelum terjadi. 
+              Platform ini didukung dengan antarmuka pengguna yang responsif dan memungkinkan akses cepat pemantauan kondisi jaringan optik dengan laporan yang mudah dipahami. Fitur manajemen data dan pelaporan memungkinkan pengguna untuk menyimpan riwayat prediksi dan mengoptimalkan proses pengambilan keputusan terkait pemeliharaan jaringan, sehingga mencegah kerusakan sebelum terjadi.
             </div>
           </div>
         </main>
