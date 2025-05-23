86822c26d4aafc77dbfdcece606cc02e094ff1d4
Vidi Ramadhandy <dhandy301102@gmail.com>
2025-05-23 23:35:17 +0700

    Update 23 Mei 2025

--- frontend/src/app/predict/altpage.js
+++ frontend/src/app/predict/altpage.js
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useAuth } from '@/lib/AuthContext';
 import ExcelJS from 'exceljs'; // Import exceljs
 import Navbar from '../../components/navbar';
 
