65124f840b0835c12cca9a6bcda71c6230fd859c
Vidi Ramadhandy <dhandy301102@gmail.com>
2025-06-12 21:36:23 +0700

    Update 12 Juni

--- backend/src/config.js
+++ backend/src/config.js
@@ -6,7 +6,7 @@ module.exports = {
   db: {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'adaptive',
     database: process.env.DB_NAME || 'optipredict_database'
   }
 };
