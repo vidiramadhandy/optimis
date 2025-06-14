8cb0809bcd21eb1ec4eec6b44720abaf652cab03
Vidi Ramadhandy <dhandy301102@gmail.com>
2025-06-14 11:55:49 +0700

    Update 14 juni

--- backend/src/config.js
+++ backend/src/config.js
@@ -5,7 +5,7 @@ module.exports = {
   jwtExpire: '24h',
   db: {
     host: process.env.DB_HOST || 'optipredict-server.mysql.database.azure.com',
-    user: process.env.DB_USER || 'admin123',
+    user: process.env.DB_USER || 'admin123@optipredict-server',
     password: process.env.DB_PASSWORD || 'adaptive11!',
     database: process.env.DB_NAME || 'optipredict_database'
   }
