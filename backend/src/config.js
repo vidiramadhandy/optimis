1e681ca74d7d93d69dd3f9783e403dd152300f07
Vidi Ramadhandy <dhandy301102@gmail.com>
2025-06-14 10:15:29 +0700

    Update 14 juni

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,9 +4,9 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'optipredict_secret',
   jwtExpire: '24h',
   db: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    host: process.env.DB_HOST || 'optipredict-server.mysql.database.azure.com',
+    user: process.env.DB_USER || 'admin123',
+    password: process.env.DB_PASSWORD || 'adaptive11!',
     database: process.env.DB_NAME || 'optipredict_database'
   }
 };
