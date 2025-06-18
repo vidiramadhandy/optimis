5eeaaa627ee971245c0d1ef3a7d750fc071601d2
Vidi Ramadhandy <dhandy301102@gmail.com>
2025-06-19 04:04:27 +0700

    allahuakbar

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-    
-export default nextConfig;
+const nextConfig = {
+  // Untuk reverse proxy
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://optipredict.my.id/api/:path*',
+      },
+    ]
+  },
+  
+  // Jika menggunakan static export
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
