287287577928893731e38267bc415fd2a983f74c
Vidi Ramadhandy <dhandy301102@gmail.com>
2025-06-19 04:06:33 +0700

    allahuakbar

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Untuk reverse proxy
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  // Konfigurasi untuk API calls ke Express backend
   async rewrites() {
     return [
       {
@@ -8,13 +15,6 @@ const nextConfig = {
         destination: 'https://optipredict.my.id/api/:path*',
       },
     ]
-  },
-  
-  // Jika menggunakan static export
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
   }
 }
 
